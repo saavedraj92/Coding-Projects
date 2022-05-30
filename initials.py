876178957f51a85922c107380b4d5fc519e67e22
Janette Saavedra <92645433+saavedraj92@users.noreply.github.com>
2022-05-30 14:29:49 -0500

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,10 @@
+# Janette Saavedra
+# I like cats.
+
+print("JJJJJ   SSS")
+print("  J    S   S")
+print("  J    S")
+print("  J     SSS")
+print("J J         S")
+print("J J     S   S")
+print(" JJ      SSS")
