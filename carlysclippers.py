8f6fd927d898fad43968670ca875ced76c2e35aa
Janette Saavedra <92645433+saavedraj92@users.noreply.github.com>
2022-06-01 19:35:30 -0500

    Create carlysclippers.py

---
+++ carlysclippers.py
@@ -0,0 +1,33 @@
+# Variables
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+total_price = 0
+
+# Loop that adds prices and stores in total_price. 
+for price in prices:
+  total_price += price
+# print(total_price)
+
+# Average price
+average_price = total_price / len(prices)
+print("Average Haircut Price ", average_price)
+
+# Average price is a little expensive. Cut all prices by 5.
+new_prices = [price -5 for price in prices]
+print(new_prices)
+
+# Last week's revenue
+total_revenue = 0
+for i in range(len(hairstyles)):
+  total_revenue += prices[i] * last_week[i]
+print("Total Revenue: ", total_revenue)
+average_daily_revenue = total_revenue / 7
+print(average_daily_revenue)
+
+# cuts under 30
+cuts_under_30 = [hairstyles[i] for i in range(len(hairstyles)) if new_prices[i] < 30]
+print(cuts_under_30 )
